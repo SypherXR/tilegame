9195b0d7b928517f00c4a9917985cec2a6340948
Reese Lynn <reeselynn28@outlook.com>
2022-01-29 18:24:29 +0000

    Update active.js

--- js/active.js
+++ js/active.js
@@ -1,1 +1,6 @@
-
+var url_string = window.location.href;
+var url = new URL(url_string);
+var time = url.searchParams.get("time");
+var score = url.searchParams.get("score");
+console.log(time);
+console.log(score);
