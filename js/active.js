b97ef17b31f26a30329d73b3439edc344e391b6b
Reese Lynn <reeselynn28@outlook.com>
2022-01-29 18:25:17 +0000

    Delete active.js

--- js/active.js
+++
@@ -1,6 +0,0 @@
-var url_string = window.location.href;
-var url = new URL(url_string);
-var time = url.searchParams.get("time");
-var score = url.searchParams.get("score");
-console.log(time);
-console.log(score);
